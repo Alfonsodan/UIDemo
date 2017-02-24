8858aa18eb544e57e9763a55ddd7e1b64c39c1bf
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-24 02:46:12 -0500

    egg timer app with full UI
    an egg timer app playing with full UI and navigation bars.

---
+++ UIDemo/UIDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UIDemo
+//
+//  Created by Danny Colangelo on 2/24/17.
+//  Copyright © 2017 Danny Colangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func cameraButton(_ sender: Any) {
+        print("button pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
